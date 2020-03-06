20376986553318381d77c6ae6c89e96c5bc1c17a
passiondev1204 <passion_dev@protonmail.com>
2020-03-06 13:34:27 +0200

    re-initing

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Viewer} from "../../components/Viewer";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <Viewer />
+    </div>
+  );
+}
+
+export default Home;
